524fb4689bd38fd828ade4cbdd7af391c5c4d648
Ikkzday <Ikkzday@3596f8e1-f4dd-4b17-bfb6-f3e4f75ec982>
2012-02-28 17:14:03 +0000

    git-svn-id: https://xp-dev.com/svn/echecEtSharp@46 3596f8e1-f4dd-4b17-bfb6-f3e4f75ec982

--- echecEtSharp/echecEtSharp/Player.cs
+++ echecEtSharp/echecEtSharp/Player.cs
@@ -10,53 +10,62 @@ namespace echecEtSharp
         public List<Piece> ListPieces { get; set; }
         public Dictionary<String, Texture2D> PieceTextures { get; set; }
 
-        private int id;
         private bool isWhitePlayer;
 
 
         public Player(int id, bool isWhitePlayer)
         {
-            this.id = id;
             this.isWhitePlayer = isWhitePlayer;
             PieceTextures = new Dictionary<String, Texture2D>();
             ListPieces = new List<Piece>();
         }
 
-        public void createPieces(string color, bool whiteC)
+        public void CreatePieces(string color, bool whiteC)
         {
-            ListPieces.Add(new Rook(PieceTextures[String.Format("{0} Rook", color)], whiteC, false));
-            ListPieces.Add(new Knight(PieceTextures[String.Format("{0} Knight", color)], whiteC, true));
-            ListPieces.Add(new Bishop(PieceTextures[String.Format("{0} Bishop", color)], whiteC, false));
-            ListPieces.Add(new Queen(PieceTextures[String.Format("{0} Queen", color)], whiteC, false));
-            ListPieces.Add(new King(PieceTextures[String.Format("{0} King", color)], whiteC, false));
-            ListPieces.Add(new Bishop(PieceTextures[String.Format("{0} Bishop", color)], whiteC, false));
-            ListPieces.Add(new Knight(PieceTextures[String.Format("{0} Knight", color)], whiteC, true));
-            ListPieces.Add(new Rook(PieceTextures[String.Format("{0} Rook", color)], whiteC, false));
+            ListPieces.Add(new Rook(PieceTextures[String.Format("{0} Rook", color)], whiteC));
+            ListPieces.Add(new Knight(PieceTextures[String.Format("{0} Knight", color)], whiteC));
+            ListPieces.Add(new Bishop(PieceTextures[String.Format("{0} Bishop", color)], whiteC));
+            ListPieces.Add(new Queen(PieceTextures[String.Format("{0} Queen", color)], whiteC));
+            ListPieces.Add(new King(PieceTextures[String.Format("{0} King", color)], whiteC));
+            ListPieces.Add(new Bishop(PieceTextures[String.Format("{0} Bishop", color)], whiteC));
+            ListPieces.Add(new Knight(PieceTextures[String.Format("{0} Knight", color)], whiteC));
+            ListPieces.Add(new Rook(PieceTextures[String.Format("{0} Rook", color)], whiteC));
 
             for (int i = 0; i < 8; i++)
             {
-                ListPieces.Add(new Pawn(PieceTextures[String.Format("{0} Pawn", color)], whiteC, false));
+                ListPieces.Add(new Pawn(PieceTextures[String.Format("{0} Pawn", color)], whiteC));
             }
 
         }
 
-        public void turnPawnIntoHulk(Case c, Pawn p, string color, bool whiteC)
+
+        public Case FindCase(Map map, Piece p)
         {
-            Queen q = new Queen(PieceTextures[String.Format("{0} Queen", color)], whiteC, false);
+            foreach (Case c in map.CaseList)
+            {
+                if (c.Piece == p)
+                    return c;
+            }
+            return null;
+        }
+
+        public void TurnIntoQueen(Case c, Pawn p, string color, bool whiteC)
+        {
+            Queen q = new Queen(PieceTextures[String.Format("{0} Queen", color)], whiteC);
             ListPieces.Remove(p);
             c.Piece = q;
             ListPieces.Add(q);
         }
 
-        public void generatePieces()
+        public void GeneratePieces()
         {
             if (isWhitePlayer)
             {
-                createPieces("White", true);
+                CreatePieces("White", true);
             }
             else
             {
-                createPieces("Black", false);
+                CreatePieces("Black", false);
             }
 
         }
